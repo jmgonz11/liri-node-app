d66f84cc28af8b0e07a455881da6d08bbcc94992
Jade Gonzales <jadegonzales@Jades-MacBook-Pro.local>
2018-04-22 14:19:41 -0500

    files

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('keys have been loaded here');
+
+exports.twitterKeys = { 
+    consumer_key: 'IcyekityuiUtUAj9JHq4o7krZ',
+    consumer_secret: '	ZWJ28FXFonSuOyv9KESaKaKFhF7lGMCZ7PtKrCg8xWWZsbPUAl',
+    access_token_key: '982302231939526656-eBhWdXzmjG0DZhTMSoCxNpoyT4G33B2',
+    access_token_secret: '9d4wCjkLPcTFC48qn8YhzmlkEjQGKdww2eHEddKWYdOLx',
+
+}
